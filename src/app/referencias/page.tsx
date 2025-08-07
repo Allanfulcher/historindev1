f7ccde5e869d0b47e03fd35f93deed242251eec9
Thomas Bocalon <tchongass1@gmail.com>
2025-08-06 22:56:02 -0300

    referencias base

---
+++ src/app/referencias/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+
+import Referencias from "../../components/Referencias";
+
+export const metadata: Metadata = {
+    title: 'Referências - Historin',
+    description: 'Explore uma ampla variedade de referências históricas, incluindo organizações, autores e obras selecionadas no Historin.',
+};
+
+export default function ReferenciasPage() {
+    return <Referencias />;
+}
