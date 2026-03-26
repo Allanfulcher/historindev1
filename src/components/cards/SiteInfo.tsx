acf620c0f62b3313e1c44df946edbca4cf05b8bf
Thomas Bocalon <tchongass1@gmail.com>
2026-03-26 12:01:00 -0300

    images

--- src/components/cards/SiteInfo.tsx
+++ src/components/cards/SiteInfo.tsx
@@ -27,7 +27,7 @@ const SiteInfo = () => {
       label: 'Brocker Turismo',
     },
     {
-      src: 'https://i.imgur.com/kYSB2cj.png',
+      src: 'https://i.imgur.com/v7R1ad2.png',
       alt: 'Hotel Alpestre',
       label: 'Hotel Alpestre',
     },
